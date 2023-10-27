aa61fac3daa1b3e8d31b5683f6d1ff1daf23d094
Olimjon-04042000 <nishonovolimjon2000@gmail.com>
2023-10-27 22:38:05 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+//  1-masala
+
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let toqSonlar = arr.filter(function(value) {
+//     return value % 2 == 1;
+// });
+// console.log(toqSonlar);
+
+
+
+// 2-masala
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let arr2 = [];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 0) {
+//         arr2.push(arr[i] ** 2);
+//     } else {
+//         arr2.push(arr[i] ** 3);
+//     }
+// }
+// console.log(arr2);
+
+
+//   3-masala
+
+
+// let arr = [1, 2, 31, 4, 61, 7, 8];
+// let karrali_5_ga;
+// let karrali_3_ga;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 3 == 0) {
+//         karrali_3_ga = "3 ga karrali son bor."
+//     } else if (arr[i] % 5 == 0) {
+//         karrali_5_ga = "5 ga karrali son bor."
+//     }
+// }
+// localStorage.setItem('karrali5', karrali_5_ga);
+// localStorage.setItem('karrali3', karrali_3_ga);
+
+
+//  7-masala
+
+
+// let n = prompt("Ixtiyoriy son kiriting: ");
+// let arr = [];
+// for (let i = 1; i <= n; i += 2) {
+//     arr.push(i);
+// }
+// console.log(arr);
+
+
+//  8-masala
+
+
+// let arr = [100, 101, 10, 125, 4, 1564, 153, 4888, 81];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 99 && arr[i] < 1000) {
+//         sum += 1;
+//     }
+// }
+// console.log(sum);
